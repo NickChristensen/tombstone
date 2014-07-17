b5937ed7042ee5ce9e14f8a387ae3877b030fce7
Nick Christensen <nick.g.christensen@gmail.com>
2014-07-16 20:07:28 -0600

    Mousewheel Direction fix: 	- Change default  to normal 	- try to detect inverted scrolling, adjust

--- assets/app.js
+++ assets/app.js
@@ -4,16 +4,16 @@
 */
 function findPayment( price, down, rate, duration){
 	var principal,
-	    monthlyRate,
-	    deno,
-	    nume,
-	    total,
-	    payment,
-	    interest;
-  price = parseFloat(price);
-  down = parseFloat(down);
-  rate = parseFloat(rate);
-  duration = parseFloat(duration);
+			monthlyRate,
+			deno,
+			nume,
+			total,
+			payment,
+			interest;
+	price = parseFloat(price);
+	down = parseFloat(down);
+	rate = parseFloat(rate);
+	duration = parseFloat(duration);
 	principal = price - down;
 	monthlyRate = (rate / 100) / 12;
 	months = duration * 12;
@@ -35,27 +35,47 @@ function findPayment( price, down, rate, duration){
 
 var myAdapter = {
 	body: document.querySelector('body'),
-  init : function ( scrubbingElement ) {},
-  start : function ( scrubbingElement ){
-  	// lock the cursor to resize
-  	this.body.style.cursor = 'ew-resize';
-  	// strip extra characters, pass number to scrubber
-    return parseInt ( scrubbingElement.node.textContent.replace(',',''), 10 );
-  },
-  change : function ( scrubbingElement, value ) {
-  	if(scrubbingElement){
-	  	value = numeral(value).format('0,0');
-	    scrubbingElement.node.textContent = value;
-	  }
-    var result = findPayment( document.getElementById('price').textContent.replace(',',''), document.getElementById('down').textContent.replace(',',''), document.getElementById('rate').textContent.replace(',',''), document.getElementById('duration').textContent.replace(',','') );
-    document.getElementById('payment').textContent = numeral(result.payment).format('0,0.00');
-    document.getElementById('total').textContent = numeral(result.total).format('0,0.00');
-  },
-  end : function ( scrubbingElement ) {
-  	// Release the cursor style
-  	this.body.style.cursor = '';
-  }
+	init : function ( scrubbingElement ) {},
+	start : function ( scrubbingElement ){
+		// lock the cursor to resize
+		this.body.style.cursor = 'ew-resize';
+		// strip extra characters, pass number to scrubber
+		return parseInt ( scrubbingElement.node.textContent.replace(',',''), 10 );
+	},
+	change : function ( scrubbingElement, value ) {
+		if(scrubbingElement){
+			value = numeral(value).format('0,0');
+			scrubbingElement.node.textContent = value;
+		}
+		var result = findPayment( document.getElementById('price').textContent.replace(',',''), document.getElementById('down').textContent.replace(',',''), document.getElementById('rate').textContent.replace(',',''), document.getElementById('duration').textContent.replace(',','') );
+		document.getElementById('payment').textContent = numeral(result.payment).format('0,0.00');
+		document.getElementById('total').textContent = numeral(result.total).format('0,0.00');
+	},
+	end : function ( scrubbingElement ) {
+		// Release the cursor style
+		this.body.style.cursor = '';
+	}
 };
+
+var myWheelDriver = (function(window, undefined){
+
+	return {
+		init : function ( scrubbingElement ) {
+			scrubbingElement.node.addEventListener("mousewheel", function ( e ) {
+				e.preventDefault();
+				var startValue = scrubbingElement.options.adapter.start ( scrubbingElement );
+				// Try to detect if natural scrolling is enabled. Only works in Safari: https://code.google.com/p/chromium/issues/detail?id=156551 Probably ok, natural scrollers prob don't use chrome much
+				var delta = e.webkitDirectionInvertedFromDevice ? startValue - e.wheelDelta : startValue + e.wheelDelta;
+				scrubbingElement.options.adapter.change ( scrubbingElement, delta, e.wheelDelta );
+			}, false);
+
+		},
+
+		remove : function ( scrubbingElement ) { }
+	};
+
+})(window);
+
 
 // Init the draggable numbers
 var draggables = document.querySelectorAll('.draggable');
@@ -64,7 +84,7 @@ for (var i = draggables.length - 1; i >= 0; i--) {
 		driver : [
 			Scrubbing.driver.Mouse,
 			Scrubbing.driver.Touch,
-			Scrubbing.driver.MouseWheel
+			myWheelDriver
 		],
 		adapter  : myAdapter,
 		resolver : Scrubbing.resolver.HorizontalProvider()
