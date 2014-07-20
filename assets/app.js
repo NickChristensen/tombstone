cc0b9e8a01f3a442d79dc6029b544c81a768d21b
Nick Christensen <nick.g.christensen@gmail.com>
2014-07-20 00:01:36 -0600

    Refactor, fixes #6

--- assets/app.js
+++ assets/app.js
@@ -17,41 +17,47 @@ function findPayment( price, down, rate, duration){
 	principal = price - down;
 	monthlyRate = (rate / 100) / 12;
 	months = duration * 12;
-	deno = 1 + monthlyRate;
-	deno = Math.pow(deno, months);
-	deno = 1 / deno;
-	deno = 1 - deno;
+	deno = 1 - (1 / Math.pow((1 + monthlyRate), months));
 	nume = principal * monthlyRate;
 
 	var result={};
 	result.payment = nume/deno;
 	result.total = (result.payment * months) + down;
 	result.interest = result.total - down - principal;
+	result.interestRatio = result.interest/result.total;
 
 	return result;
 }
 
 var myAdapter = {
 	body: document.querySelector('body').style,
-	init : function ( scrubbingElement ) {},
+	init : function ( scrubbingElement ) {
+		this.price = document.getElementById('price').textContent.replace(/,/g,'');
+		this.down = document.getElementById('down').textContent.replace(/,/g,'');
+		this.rate = document.getElementById('rate').textContent.replace(/,/g,'');
+		this.duration = document.getElementById('duration').textContent.replace(/,/g,'');
+	},
 	start : function ( scrubbingElement ){
+		// Determine if we need decimals
+		this.formatString = scrubbingElement.node.dataset.decimals ? '0,0.00' : '0,0';
+		// strip extra characters
 		var initValue = parseFloat ( scrubbingElement.node.textContent.replace(/,/g,'') );
+		// don't let it be 0
 		initValue = initValue ? initValue : 1;
 		// lock the cursor to resize
 		this.body.cursor = 'ew-resize';
 		// Variable Resistance based on starting number
 		scrubbingElement.options.resolver.divider = 1000/initValue;
-		// strip extra characters, pass number to scrubber
 		return initValue;
 	},
 	change : function ( scrubbingElement, value ) {
 		if(scrubbingElement){
-			var decimals = scrubbingElement.node.dataset.decimals;
-			var formatString = decimals ? '0,0.00' : '0,0';
-			formattedValue = numeral(value).format(formatString);
+			formattedValue = numeral(value).format(this.formatString);
 			scrubbingElement.node.textContent = formattedValue;
+			this[scrubbingElement.node.id] = value;
 		}
-		var result = findPayment( document.getElementById('price').textContent.replace(/,/g,''), document.getElementById('down').textContent.replace(/,/g,''), document.getElementById('rate').textContent.replace(/,/g,''), document.getElementById('duration').textContent.replace(/,/g,'') );
+
+		var result = findPayment( this.price, this.down, this.rate, this.duration );
 		document.getElementById('payment').textContent = numeral(result.payment).format('0,0.00');
 		document.getElementById('total').textContent = numeral(result.total).format('0,0.00');
 	},
